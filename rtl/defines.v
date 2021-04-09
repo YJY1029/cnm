c7905c4f680d9b7f4f56ab6e0ff7c8c5d7298ea0
YJY1029 <839665724@qq.com>
2021-04-09 17:13:01 +0800

    defines almost completed; add blank flip-flops; doing id module

--- rtl/defines.v
+++ rtl/defines.v
@@ -3,7 +3,7 @@
 `define JUMP 1b'1
 `define HOLD 1b'1
 
-//Instruction
+//Instruction parameters
 `define INST_WIDTH 31:0
 `define INST_ADDR_WIDTH 31:0
 `define INI_INST_ADDR 32h'0
@@ -33,3 +33,81 @@
 `define minstreth 
 `define msip 
 */
+
+//LUI
+`define LUI		7b'0110111
+
+//AUIPC
+`define AUIPC 7b'0010111
+
+//JAL and JALR
+`define JAL 		7b'1101111
+`define JALR 	7b'1100111
+
+//B format *6
+`define B_FORMAT	7b'1100011
+`define BEQ 3b'000
+`define BNE 3b'001
+`define BLT 3b'100
+`define BGE 3b'101
+`define BLTU 3b'110
+`define BGEU 3b'111
+
+//I format, loads *5
+`define IL_FORMAT 7b'0000011
+`define LB 3b'000
+`define LH 3b'001
+`define LW 3b'010
+`define LBU 3b'100
+`define LHU 3b'101
+
+//S format *3
+`define S_FORMAT 7b'0100011
+`define SB 3b'000
+`define SH 3b'001
+`define SW 3b'010
+
+//I format *9
+`define I_FORMAT 7b'0010011
+`define ADDI 3b'000
+`define SLTI 3b'010
+`define SLTIU 3b'011
+`define XORI 3b'100
+`define ORI 3b'110
+`define ANDI 3b'111
+`define SLLI 3b'001
+`define SRLI_SRAI 3b'101
+	`define SRLI 7b'0000000
+	`define SRAI 7b'0100000
+
+//R format *10
+`define R_FORMAT 7b'0110011
+`define ADD_SUB 3b'000
+	`define ADD 7b'0000000
+	`define SUB 7b'0100000
+`define SLL 3b'001
+`define SLT 3b'010
+`define SLTU 3b'011
+`define XOR 3b'101
+`define SRL_SRA 3b'101
+	`define SRL 7b'0000000
+	`define SRA 7b'0100000
+`define OR 3b'110
+`define AND 3b'111
+
+//FENCE and FENCE.I
+`define FENCE_FENCEI 7b'0001111
+`define FENCE 3b'000
+`define FENCEI 3b'001
+
+//Environment and CSRs *8
+`define ENVIR_CSR 7b'1110011
+`define ENVIR 3b'000
+	`define ECALL 12b'0
+	`define EBREAK 12b'1
+`define CSRRW 3b'001
+`define CSRRS	3b'010
+`define CSRRC 3b'011
+`define CSRRWI 3b'101
+`define CSRRSI 3b'110
+`define CSRRCI 3b'111
--- rtl/id.v
+++ rtl/id.v
@@ -24,6 +24,7 @@ module id(
 	output wire un_signed_o 
 	);
 	
+	//Instruction breakup
 	wire [6:0] opcode = inst[6:0]; 
 	wire [`REG_ADDR_WIDTH] rd = inst[11:7];
 	wire [2:0] funct3 = inst[14:12];
@@ -32,4 +33,84 @@ module id(
 	wire [6:0] funct7 = inst[31:25]; 
 	
 	assign inst_addr_o = inst_addr; 
+	
+	/*
+	//opcode
+	wire lui = (opcode == `LUI); 
+	wire auipc = (opcode == `AUIPC); 
+	wire jal = (opcode == `JAL); 
+	wire jalr = (opcode == `JALR); 
+	wire b_format = (opcode == `B_FORMAT); 
+	wire il_format = (opcode == `IL_FORMAT); 
+	wire s_format = (opcode == `S_FORMAT); 
+	wire i_format = (opcode == `I_FORMAT); 
+	wire r_format = (opcode == `R_FORMAT); 
+	wire fence_fencei = (opcode == `FENCE_FENCEI); 
+	wire envir_csr = (opcode == `ENVIR_CSR); 
+	
+	//B format
+	wire beq = b_format & (funct3 == `BEQ); 
+	wire bne = b_format & (funct3 == `BNE); 
+	wire blt = b_format & (funct3 == `BLT); 
+	wire bge = b_format & (funct3 == `BGE); 
+	wire bltu = b_format & (funct3 == `BLTU); 
+	wire bgeu = b_format & (funct3 == `BGEU); 
+	
+	//I format loads
+	wire lb = il_format & (funct3 == `LB); 
+	wire lh = il_format & (funct3 == `LH); 
+	wire lw = il_format & (funct3 == `LW); 
+	wire lbu = il_format & (funct3 == `LBU); 
+	wire lhu = il_format & (funct3 == `LHU); 
+	
+	//S format
+	wire sb = s_format & (funct3 == `SB); 
+	wire sh = s_format & (funct3 == `SH); 
+	wire sw = s_format & (funct3 == `SW); 
+	
+	//I format 
+	wire addi = i_format & (funct3 == `ADDI); 
+	wire slti = i_format & (funct3 == `SLTI); 
+	wire sltiu = i_format & (funct3 == `SLTIU); 
+	wire xori = i_format & (funct3 == `XORI); 
+	wire ori = i_format & (funct3 == `ORI); 
+	wire andi = i_format & (funct3 == `ANDI); 
+	wire slli = i_format & (funct3 == `SLLI); 
+	wire srli_srai = i_format & (funct3 == `SRLI_SRAI); 
+		wire srli = srli_srai & (funct7 == `SRLI); 
+		wire srai = srli_srai & (funct7 == `SRAI); 
+	
+	//R format 
+	wire add_sub = r_format & (funct3 == `ADD_SUB); 
+		wire add = add_sub & (funct7 == `ADD); 
+		wire sub = add_sub & (funct7 == `SUB); 
+	wire sll = r_format & (funct3 == `SLL); 
+	wire slt = r_format & (funct3 == `SLT); 
+	wire sltu = r_format & (funct3 == `SLTU); 
+	wire xorr = r_format & (funct3 == `XOR); //avoiding keyword xor
+	wire srl_sra = r_format & (funct3 == `SRL_SRA); 
+		wire srl = srl_sra & (funct7 == `SRL); 
+		wire sra = srl_sra & (funct7 == `SRA); 
+	wire orr = r_format & (funct3 == `OR); //avoiding keyword or
+	wire andd = r_format & (funct3 == `AND); //avoiding keyword and
+	
+	//FENCE and FENCE.I
+	wire fence = fence_fencei & (funct3 == `FENCE); 
+	wire fencei = fence_fencei & (funct3 == `FENCEI); 
+	
+	//Environment and CSRs
+	wire envir = envir_csr & (funct3 == `ENVIR);
+		wire ecall = envir & (funct7 == `ECALL); 
+		wire ebreak = envir & (funct7 == `EBREAK); 
+	wire csrrw = envir_csr & (funct3 == `CSRRW);
+	wire csrrs = envir_csr & (funct3 == `CSRRS);
+	wire csrrc = envir_csr & (funct3 == `CSRRC);
+	wire csrrwi = envir_csr & (funct3 == `CSRRWI);
+	wire csrrsi = envir_csr & (funct3 == `CSRRSI);
+	wire csrrci = envir_csr & (funct3 == `CSRRCI);
+	*/
+	
+	assign rs1_addr_o = (jalr | b_format | il_format | s_format | I_format | r_format | csrrw | csrrs | csrrc) ? rs1; 
+	assign rs2_addr_o = (
+	
 endmodule
