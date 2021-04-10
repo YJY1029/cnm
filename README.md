5d174f474094cd6766a428b6f9496b95b74a9076
YJY1029 <839665724@qq.com>
2021-04-10 23:07:32 +0800

    csregfile interfaces preliminarily determined; defines formatted; standard dff module to be implemented

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
+
 # Core of Normal Microarchitecture
 This project is an RV32I core of normal, simple and classical microarchitecture. 
 This first version of design is very simple, focusing on simplicity and readability rather than performance, as it only has a three-stage pipeline, which is divided into instruction fetch, instruction decoding and execution. 
 
-## Principles
+## Design Ideas
 
-When implementing this design, three main principles are held: 
+When implementing this design, there are three main design ideas: 
 
  1. *Classical*. A three-stage classical pipeline is implemented and no abundant optimization has been done. For any branch instruction, the pipeline simply holds. 
  2. *Simple*. Similar modules are integrated, which unifies their functions and minimizes the number of interfaces. 
@@ -13,15 +14,18 @@ When implementing this design, three main principles are held:
 ## Modules
 
 Basically our core contains: 
-1. **pc** : Programme counter. It's used to indicate the address of current instruction. Global clk signal and rst signal (both are connected to almost every module), along with hold signal and jump signal are connected to our pc module. If none of these signals request pc to change its address, then after each tick it points to the next instruction address, which is exactly one word, or 4 bytes after current address. Most importantly, after deciding the address we are about to use, pc fetches the instruction from *sb* and passes it into *id*. 
+1. ***defines***: Relevant parameters, configure, and necessary information of decoding. 
+2. ***pc*** : Programme counter. It's used to indicate the address of current instruction. Global rst signal (connected to almost every module), along with hold signal and jump signal are connected to our pc module. If none of these signals request pc to change its address, then it points to the next instruction address, which is exactly one word, or four bytes after current address. Most importantly, after deciding the address we are about to use, *pc* fetches the instruction from *sb* and passes it into *id*. 
 
-2. ***id***: Instruction decoder. It's used to decode instructions from previous instruction fetch stage, using a completely logical circuit. After this it passes relevant information to *excutrol*.  
-3. ***csregfile***: Control and status registers and regfile. 
-4. ***executrol***: Execution and control. 
-5. ***Two fliops***: Flip-flops between each pipeline stage. 
-6. ***sb***: A simple bus. 
-7. ***CoNM***: Top module. 
+3. ***id***: Instruction decoder. It's used to decode instructions from previous instruction fetch stage, using a completely combinational logical circuit. After this, while it passes register addresses to *csregfile*, informing it to output data to *executrol*, it also passes relevant information to *excutrol*.  
+4. ***csregfile***: Control and status registers and regfile. CSRs (control and status registers) and register file are integrated in this design, due to the obvious similarity between them, and they are in charge of reading and writing of these registers. From *id* it receives addresses of registers that are to be read by *executrol*, and outputs data in rs1 and/or rs2 or specific CSR; From executrol it receives we (write enable) signal, write address and write data of specific rd or CSR, and puts the data into where it belong. 
+5. ***executrol***: Execution and control. Given that execution results are tightly coherent to control function, in this design we also integrated execution and control modules into *executrol*. This module performs all calculations, including add, sub, shift, etc., and all control-related operations, including jump, branch, hold, etc. In all, it performs functions referring to the information inputted from *id*, and control-related part processes its results. Finally, *executrol* outputs relevant signals along with operation results. 
+6. ***Two fliops***: D flip-flops between each pipeline stage. They are both very simple D flip-flops: all data and signals inputted from previous stage are kept here for one tick and then sent to the next stage. Note that when hold signal is set, these two flip-flops will continuously perform NOP instruction until hold flag is off. 
+7. ***sb***: A simple bus. Its details are to be determined, but in all it's a very simple bus connected to *pc*, *executrol* and the most significant peripheral *RAM*, transmitting instruction and data. 
+8. ***CoNM***: Top module. All submodules are instantiated here, with several peripherals and global signals connected to it.
 
 ## Coding Style
-
-*To be continued*
+To enhance readability and coding convenience, several coding principles should be clarified: 
+ - Clean and simple macro definitions and variable naming,  e.g. non-suffix input variables and instruction names, -r-suffixed register variables and -o-suffixed output variables, enhancing readability and coding convenience. 
+ - Using *assign* statements as conditionals, as to avoid uncertain states and priority selection circuits under some circumstances. 
+ - Initiating standard D flip-flop modules implemented elsewhere, which will greatly benefit implementation process as well. 
--- rtl/defines.v
+++ rtl/defines.v
@@ -1,35 +1,35 @@
 //Flags
-`define RST 1'b0
-`define JUMP 1'b1
-`define HOLD 1'b1
-`define WRITE_ENABLE 1'b1
-
-//Instruction parameters
-`define INST_WIDTH 31:0
-`define INST_ADDR_WIDTH 31:0
-`define INI_INST_ADDR 32'h0
-
-//General integer register
-`define REG_WIDTH 31:0
-`define REG_ADDR_WIDTH 4:0
-`define ZERO_REG 5'h0
+`define RST              1'b0
+`define JUMP             1'b1
+`define HOLD             1'b1
+`define WRITE_ENABLE	   1'b1
+                             
+//Instruction parameters                
+`define INST_WIDTH		   31:0
+`define INST_ADDR_WIDTH  31:0
+`define INI_INST_ADDR   32'h0
+                             
+//General integer register           
+`define REG_WIDTH			   31:0
+`define REG_ADDR_WIDTH    4:0
+`define ZERO_REG 			   5'h0
 
 //CSRs
 //others might be needed while some below might be useless
 `define CSR_ADDR_WIDTH 11:0
 
-`define mstatus 12'h300 //*very complex
-`define misa 12'h301 //32'h40020000
-`define mie 12'h304//mstatus and mask related?
-`define mtvec 12'h305//trap address
-`define mscratch 12'h340//temporary register data saving before exception
-`define mepc 12'h341//the pc address before exception
-`define mcause 12'h342//exception cause indicator
-`define mtval 12'h343//opcode or memory address before exception
-`define mip 12'h344//pending related?
-`define mcycle 12'hb00//counting cycle times
-`define mcycleh 12'hb80
-`define mvendorid 12'hf11 //32'h13109f5
+`define mstatus    12'h300	//*very complex                                       
+`define misa       12'h301	//32'h40020000                                        
+`define mie 	     12'h304	//mstatus and mask related?                           
+`define mtvec      12'h305	//trap address                                        
+`define mscratch   12'h340	//temporary register data saving before exception     
+`define mepc			 12'h341	//the pc address before exception                     
+`define mcause 		 12'h342	//exception cause indicator                           
+`define mtval 		 12'h343	//opcode or memory address before exception           
+`define mip 			 12'h344	//pending related?                                    
+`define mcycle 		 12'hb00	//counting cycle times                                
+`define mcycleh 	 12'hb80                                                        
+`define mvendorid  12'hf11 	//32'h13109f5                                         
 /*These might be totally useless
 `define minstret 
 `define minstreth 
@@ -37,83 +37,83 @@
 */
 
 //NOP
-`define NOP 32h'1b
+`define NOP 			32h'1b
 
 //LUI
-`define LUI		7'b0110111
+`define LUI				7'b0110111
 
 //AUIPC
-`define AUIPC 7'b0010111
+`define AUIPC 		7'b0010111
 
 //JAL and JALR
-`define JAL 		7'b1101111
-`define JALR 	7'b1100111
+`define JAL 			7'b1101111
+`define JALR 			7'b1100111
 
 //B format *6
-`define B_FORMAT	7'b1100011
-`define BEQ 3'b000
-`define BNE 3'b001
-`define BLT 3'b100
-`define BGE 3'b101
-`define BLTU 3'b110
-`define BGEU 3'b111
-
-//I format, loads *5
-`define IL_FORMAT 7'b0000011
-`define LB 3'b000
-`define LH 3'b001
-`define LW 3'b010
-`define LBU 3'b100
-`define LHU 3'b101
-
-//S format *3
-`define S_FORMAT 7'b0100011
-`define SB 3'b000
-`define SH 3'b001
-`define SW 3'b010
-
-//I format *9
-`define I_FORMAT 7'b0010011
-`define ADDI 3'b000
-`define SLTI 3'b010
-`define SLTIU 3'b011
-`define XORI 3'b100
-`define ORI 3'b110
-`define ANDI 3'b111
-`define SLLI 3'b001
-`define SRLI_SRAI 3'b101
-	`define SRLI 7'b0000000
-	`define SRAI 7'b0100000
-
-//R format *10
-`define R_FORMAT 7'b0110011
-`define ADD_SUB 3'b000
-	`define ADD 7'b0000000
-	`define SUB 7'b0100000
-`define SLL 3'b001
-`define SLT 3'b010
-`define SLTU 3'b011
-`define XOR 3'b101
-`define SRL_SRA 3'b101
-	`define SRL 7'b0000000
-	`define SRA 7'b0100000
-`define OR 3'b110
-`define AND 3'b111
+`define B_FORMAT	7'b1100011            
+`define BEQ 			3'b000                
+`define BNE 			3'b001                
+`define BLT 			3'b100                
+`define BGE 			3'b101                
+`define BLTU 			3'b110                
+`define BGEU 			3'b111                
+                                        
+//I format, loads *5                    
+`define IL_FORMAT 7'b0000011            
+`define LB 				3'b000                
+`define LH 				3'b001                
+`define LW 				3'b010                
+`define LBU 			3'b100                
+`define LHU 			3'b101                
+                                        
+//S format *3                           
+`define S_FORMAT 	7'b0100011            
+`define SB 				3'b000                
+`define SH 				3'b001                
+`define SW 				3'b010                
+                                        
+//I format *9                           
+`define I_FORMAT 	7'b0010011            
+`define ADDI 			3'b000                
+`define SLTI 			3'b010                
+`define SLTIU 		3'b011                
+`define XORI 			3'b100                
+`define ORI 			3'b110                
+`define ANDI			3'b111                
+`define SLLI 			3'b001                
+`define SRLI_SRAI 3'b101                
+	`define SRLI 		7'b0000000            
+	`define SRAI 		7'b0100000            
+                                        
+//R format *10                          
+`define R_FORMAT 	7'b0110011            
+`define ADD_SUB 	3'b000                
+	`define ADD 		7'b0000000            
+	`define SUB			7'b0100000            
+`define SLL 			3'b001                
+`define SLT 			3'b010                
+`define SLTU 			3'b011                
+`define XOR 			3'b101                
+`define SRL_SRA 	3'b101                
+	`define SRL 		7'b0000000            
+	`define SRA 		7'b0100000            
+`define OR 				3'b110                
+`define AND 			3'b111                
 
 //FENCE and FENCE.I
-`define FENCE_FENCEI 7'b0001111
-`define FENCE 3'b000
-`define FENCEI 3'b001
-
-//CSRs *8
-`define CSR 7'b1110011
-`define CSRRW 3'b001
-`define CSRRS	3'b010
-`define CSRRC 3'b011
-`define CSRRWI 3'b101
-`define CSRRSI 3'b110
-`define CSRRCI 3'b111
-
-//Environment *2
-`define ECALL 32'h73
-`define EBREAK 32'h100073
+`define FENCE_FENCEI	7'b0001111
+`define FENCE 		3'b000        
+`define FENCEI 		3'b001        
+                                
+//CSRs *8                       
+`define CSR 			7'b1110011    
+`define CSRRW 		3'b001        
+`define CSRRS			3'b010        
+`define CSRRC 		3'b011        
+`define CSRRWI 		3'b101        
+`define CSRRSI 		3'b110        
+`define CSRRCI		3'b111        
+                                
+//Environment *2                
+`define ECALL 		32'h73        
+`define EBREAK 		32'h100073    
